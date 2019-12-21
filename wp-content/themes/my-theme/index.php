36b881036470a212bc2832f25276bb3d47fd05b8
GombertNoluene <noluene.gombert@gmail.com>
2019-12-21 17:41:51 +0100

    Rendu de toutes les modifications effectuées

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -17,11 +17,26 @@ if ( have_posts() ) {
      * Il existe d'autres méthodes, par exemple pour afficher le Titre du contenu, on peut utiliser la méthode the_title()
      */
     the_content();
+    //IMAGES
     $banner = get_field('banner_background_image');
     $bandeau = get_field('conference_img');
     $programme = get_field('programme_img');
     $orateur_img = get_field('orateur_repeteur_image');
     $information = get_field('information_img');
+    $video1 = get_field('video_content_image');
+    $video2 = get_field('video_content_image_2');
+    $video3 = get_field('video_content_image_3');
+    $video4 = get_field('video_content_image_4');
+    $video5 = get_field('video_content_image_5');
+    $video6 = get_field('video_content_image_6');
+
+    //LIENS
+    $banner_lien = get_field('banner_register_link');
+    $programme_lien = get_field('programme_register_link');
+    //REPETEURS
+    $orateurs = get_field('orateur_contact');
+    $actus = get_field('actus_repeteur');
+
   }
 }
 
@@ -32,19 +47,20 @@ if ( have_posts() ) {
   <p class="banner_baseline"><?php the_field('banner_baseline'); ?></p>
   <p class="banner_title_brown"><?php the_field('banner_title_brown'); ?></p>
   <p class="banner_title_green"><?php the_field('banner_title_green'); ?></p>
-  <a class="banner_register_link"><?php the_field('banner_register_link'); ?></a>
+  <a class="banner_register_link" href="<?php echo $banner_lien['url']?>"><?php echo $banner_lien['title']; ?></a>
 </section>
 
 
 <section class="conference">
-  <p class="conference_title"><?php the_field('conference_title'); ?></p>
+  <p class="title"><?php the_field('conference_title'); ?></p>
   <p class="conference_texte"><?php the_field('conference_texte'); ?></p>
   <img class="conference_img"src="<?php echo $bandeau['url']; ?>" alt="" />
 </section>
 
 
 <section class="programme">
-  <p class="programme_title"><?php the_field('programme_title'); ?></p>
+
+  <p class="title"><?php the_field('programme_title'); ?></p>
 
   <div class="programme_grid">
     <div class="left">
@@ -73,18 +89,18 @@ if ( have_posts() ) {
         </div>
     </div>
   </div>
-  <a class="programme_register_link"><?php the_field('programme_register_link'); ?></a>
+  <a class="programme_register_link" href="<?php echo $programme_lien['url'] ?>"><?php echo $programme_lien['title'] ?></a>
   <img class="programme_img"src="<?php echo $programme['url']; ?>" alt="" />
 </section>
 
 
 <section class="orateur">
-  <p class="orateur_title"><?php the_field('orateur_title'); ?></p>
+  <p class="title"><?php the_field('orateur_title'); ?></p>
   <p class="orateur_texte"><?php the_field('orateur_texte'); ?></p>
 
   <div class="orateur_flex">
     <?php 
-        $orateurs = get_field('orateur_contact'); 
+         
         foreach ($orateurs as $orateur){
           //var_dump($orateur);
           //$orateur['orateur_repeteur_image']['url'];
@@ -92,19 +108,17 @@ if ( have_posts() ) {
           echo "<img class='orateur_repeteur_image' src=".$orateur['orateur_repeteur_image']['url']." />";
           echo "<p class='orateur_repeteur_nom'>".$orateur['orateur_repeteur_nom']."</p>";
           echo "<p class='orateur_repeteur_description'>".$orateur['orateur_repeteur_description']."</p>";
-          echo "<div class='orateur_repeteur_video'><a>".$orateur['orateur_repeteur_video']['title']."</a></div>";
+          echo "<div class='orateur_repeteur_video'><a href=".$orateur['orateur_repeteur_video']['url'].">".$orateur['orateur_repeteur_video']['title']."</a></div>";
           echo "</div>";
         }
       ?>
   </div>
 
-
 </section>
 
 
-<p class="information_title"><?php the_field('information_title'); ?></p>
-
-<section class="information" style="background-image: url('<?php echo $information['url']; ?>'); height: 100vh; background-repeat: no-repeat; background-size: cover; margin: 5% 5%;">
+  <p class="title"><?php the_field('information_title'); ?></p>
+<section class="informations_pratiques" style="background-image: url('<?php echo $information['url']; ?>'); height: 100vh; background-repeat: no-repeat; background-size: cover; margin: 5% 5%;">
   <div class="information_vert">
     <div class="information_vert_adresse">
       <img class="picto" src="<?php echo get_template_directory_uri(); ?>/assets/img/picto-map.svg" alt="">
@@ -119,10 +133,71 @@ if ( have_posts() ) {
       <p class="information_restaurant"><?php the_field('information_restaurant'); ?></p>
     </div>
   </div>
-  <p class="information_carte"><?php the_field('information_carte'); ?></p>
-
-
-
+  <div class="information_blanc">
+    <p class="information_carte"><?php the_field('information_carte'); ?></p>
+  </div>
 </section>
+
+
+<section class="video">
+
+  <p class="title"><?php the_field('video_title'); ?></p>
+  <div class="video_content">
+
+    <div class="video_grid_1">
+      <div class="v1">
+        <img class="video_image"src="<?php echo $video1['url']; ?>" alt="" />
+        <p class="video_title"><?php the_field('video_content_title'); ?></p>
+        <p class="video_texte"><?php the_field('video_content_texte'); ?></p>
+      </div>
+      <div class="v1">
+        <img class="video_image"src="<?php echo $video2['url']; ?>" alt="" />
+        <p class="video_title"><?php the_field('video_content_title_2'); ?></p>
+        <p class="video_texte"><?php the_field('video_content_texte_2'); ?></p>
+      </div>
+    </div>
+    <div class="video_grid_2">
+      <img class="video_image_2"src="<?php echo $video3['url']; ?>" alt="" />
+      <div class="v2">
+        <p class="video_title_2"><?php the_field('video_content_title_3'); ?></p>
+        <p class="video_texte_2"><?php the_field('video_content_texte_3'); ?></p>
+      </div>
+      <img class="video_image_2"src="<?php echo $video4['url']; ?>" alt="" />
+      <div class="v2">
+        <p class="video_title_2"><?php the_field('video_content_title_4'); ?></p>
+        <p class="video_texte_2"><?php the_field('video_content_texte_4'); ?></p>
+      </div>
+      <img class="video_image_2"src="<?php echo $video5['url']; ?>" alt="" />
+      <div class="v2">
+        <p class="video_title_2"><?php the_field('video_content_title_5'); ?></p>
+        <p class="video_texte_2"><?php the_field('video_content_texte_5'); ?></p>
+      </div>
+      <img class="video_image_2"src="<?php echo $video6['url']; ?>" alt="" />
+      <div class="v2">
+        <p class="video_title_2"><?php the_field('video_content_title_6'); ?></p>
+        <p class="video_texte_2"><?php the_field('video_content_texte_6'); ?></p>
+      </div>
+    </div>
+</section>
+
+
+<section class="actus">
+  <p class="title"><?php the_field('actus_title'); ?></p>
+
+      <?php 
+         
+        foreach ($actus as $actu){
+          //var_dump($actu);
+          //$actu['actus_repeteur_image'];
+
+          //echo "<div class='actus_articles'>";
+          // echo "<img class='orateur_repeteur_image' src=".$orateur['orateur_repeteur_image']['url']." />";
+          //echo "<p class='actus_repeteur'>".$actu['actus_repeteur_image']['post_title']."</p>";
+          // echo "<p class='orateur_repeteur_description'>".$orateur['orateur_repeteur_description']."</p>";
+          // echo "<div class='orateur_repeteur_video'><a href=".$orateur['orateur_repeteur_video']['url'].">".$orateur['orateur_repeteur_video']['title']."</a></div>";
+          // echo "</div>";
+        }
+      ?>
+
 
 <?php get_footer(); ?>
